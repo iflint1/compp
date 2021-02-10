b827728ca76fb39b65cefca75f8c0cf65a6bb6d7
Ian Flint <ian.flint@unimelb.edu.au>
2021-02-11 10:38:40 +1100

    Implement simulation of homogeneous COM-Poisson point process

---
+++ R/rcompp.R
@@ -0,0 +1,29 @@
+#' Homogeneous COM-Poisson point process simulation
+#'
+#' @param n Number of replications.
+#' @param lambda Quasi-intensity.
+#' @param nu Dispersion parameter
+#' @param window Observation window.
+#' @param drop Logical. If n = 1 and drop = TRUE (the default), the result will be a point pattern,
+#' rather than a list containing a single point pattern.
+#'
+#' @return List of `spatstat::ppp` samples of a homogeneous COM-Poisson point process.
+#'
+#' @import COMPoissonReg
+#' @importFrom spatstat area.owin owin runifpoint
+#' @export
+rcomppp <- function(n = 1,
+                    lambda = 1,
+                    nu = 1,
+                    window = owin(),
+                    drop = TRUE) {
+  area <- area.owin(window)
+  total_points <- COMPoissonReg::rcmp(n = n, lambda = lambda * area, nu = nu)
+  result <- lapply(total_points, function(npoints) {
+    runifpoint(n = npoints, win = window)
+  })
+  if(length(result) == 1 && drop == TRUE) {
+    result <- result[[1]]
+  }
+  result
+}
